559ff87c85870b5ddbced7f1ccf2fa132488a406
aronm99 <markanda@mcmaster.ca>
2023-03-17 23:34:41 -0400

    Update app styling

--- src/App.tsx
+++ src/App.tsx
@@ -2,56 +2,52 @@ import React, {useState, useEffect, useCallback} from 'react';
 import { Routes, Route, Link, Outlet } from 'react-router-dom';
 import useWebSocket from 'react-use-websocket';
 import MainPage from './features/MainPage/MainPage';
-import { SingleUGVPage } from './features/SingleUGV/SingleUGV';
-import UGVMotorVel from './features/SingleUGVDiag/MotorVel';
-import UGVMotorDist from './features/SingleUGVDiag/MotorDist';
+import SingleUGVMenu from './features/SingleUGV/SingleUGVMenu';
+import SingleUGVPage from './features/SingleUGV/SingleUGV';
+import UGVMotorVel from './features/SingleUGV/SingleUGVDiag/MotorVel';
+import UGVMotorDist from './features/SingleUGV/SingleUGVDiag/MotorDist';
 import './App.css';
-import { Button } from 'antd';
+import { ConfigProvider, theme } from 'antd';
+
+const antdTheme = {
+  algorithm: theme.darkAlgorithm,
+  token:{
+    colorPrimary: '#00875a',
+    colorBgBase: '#24292e',
+    borderRadius: 3
+  }  
+}
 
 function App() {
-  
-  // const [socket, setSocket] = useState<Socket|null>(null);
-  // const WS_URL = 'ws://127.0.0.1:63733';
-  
+    
   const [socketUrl, setSocketUrl] = useState('ws://127.0.0.1:63733');
   const [messageHistory, setMessageHistory] = useState<any[]>([]);
 
   const { sendMessage, lastMessage, readyState } = useWebSocket(socketUrl);
 
+
+
   useEffect(() => {
     if (lastMessage !== null) {
       setMessageHistory((prev) => prev.concat(lastMessage));
-      console.log(lastMessage.data);
-      sendMessage("Hello");
     }
 
   }, [lastMessage, setMessageHistory]);
 
-  const onClick = useCallback(()=>{
-    console.log("Send message to UGV1");
-    sendMessage("helloooo");
-  },[]);
 
-  // useWebSocket(WS_URL, {
-  //   onOpen: () => {
-  //     console.log('WebSocket connection established.');
-  //   },
-  //   onMessage: (event)=>{
-  //     console.log(event.data);
-  //   }
-  // });
 
   return (
     <div className="App">
-      <Button title='Button' onClick={onClick}>
-        Start UGV
-      </Button>
-      <Routes>
-        <Route path='/' element={<MainPage sendMessage={sendMessage}/>}/>
-        <Route path='/ugv' element={<SingleUGVPage/>} />
-        <Route path='/ugv/diag/vel' element={<UGVMotorVel/>}/>
-        <Route path='/ugv/diag/dist' element={<UGVMotorDist/>}/>
-      </Routes>
+      <ConfigProvider theme={antdTheme}>
+        <Routes>
+          <Route path='/' element={<MainPage sendMessage={sendMessage}/>}/>
+          <Route path='ugv' element={<SingleUGVMenu sendMessage={sendMessage}/>}>
+            <Route path='path' element={<SingleUGVPage/>}/>
+            <Route path='diag/vel' element={<UGVMotorVel/>}/>
+            <Route path='diag/dist' element={<UGVMotorDist/>}/>
+          </Route>
+        </Routes>
+      </ConfigProvider>
     </div>
   );
 }
